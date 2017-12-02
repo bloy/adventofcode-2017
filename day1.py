210cd6483625e49dc5f98f6619e2397150a4783f
Mike Bloy <mike@bloy.org>
2017-12-01 22:56:03 -0600

    refactor day1

--- day1.py
+++ day1.py
@@ -1,22 +1,17 @@
 #!env python
 
-with open('day1_input.txt') as f:
-    puzzle_input = f.read()
-    puzzle_input = puzzle_input.strip()
 
-summable1 = []
-summable2 = []
-index1 = 1
-index2 = len(puzzle_input) // 2
+def revcaptcha(rotate, seed):
+    return sum(int(digit) for i, digit in enumerate(seed)
+               if seed[(i+rotate) % len(seed)] == digit)
 
-for i, digit in enumerate(puzzle_input):
-    if puzzle_input[(i+index1) % len(puzzle_input)] == digit:
-        summable1.append(int(digit))
-    if puzzle_input[(i+index2) % len(puzzle_input)] == digit:
-        summable2.append(int(digit))
+if __name__ == '__main__':
+    with open('day1_input.txt') as f:
+        puzzle_input = f.read()
+        puzzle_input = puzzle_input.strip()
 
-print(summable1)
-print(sum(summable1))
+    rotate1 = 1
+    rotate2 = len(puzzle_input) // 2
 
-print(summable2)
-print(sum(summable2))
+    print(revcaptcha(rotate1, puzzle_input))
+    print(revcaptcha(rotate2, puzzle_input))
