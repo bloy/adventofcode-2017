a863febbd1b8fd28022084178a4bf6d330b35da2
Mike Bloy <mike@bloy.org>
2017-12-03 23:13:32 -0600

    day4 both solutions

--- day4.py
+++ day4.py
@@ -2,13 +2,34 @@
 
 import aoc
 
+import itertools
+
 
 def solve1(rows):
-    return rows
+    count = 0
+    for row in rows:
+        words = row.split(' ')
+        if len(words) == len(set(words)):
+            count += 1
+    return count
+
+
+def valid2(phrase):
+    wordset = set()
+    for word in phrase.split(' '):
+        if word in wordset:
+            return False
+        for scramble in itertools.permutations(word):
+            wordset.add("".join(scramble))
+    return True
 
 
 def solve2(rows):
-    return rows
+    count = 0
+    for row in rows:
+        if valid2(row):
+            count += 1
+    return count
 
 
 if __name__ == '__main__':
