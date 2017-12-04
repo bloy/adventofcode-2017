088fe524c7f882351b078729e7cc841045dd339d
Mike Bloy <mike@bloy.org>
2017-12-03 22:58:18 -0600

    day4 setup

---
+++ day4.py
@@ -0,0 +1,17 @@
+#!env python
+
+import aoc
+
+
+def solve1(rows):
+    return rows
+
+
+def solve2(rows):
+    return rows
+
+
+if __name__ == '__main__':
+    rows = [row for row in aoc.input_lines(day=4)]
+    print(solve1(rows))
+    print(solve2(rows))
