c6c0a203ca424e15b4c046d38fc27325a0f3b711
Mike Bloy <mike@bloy.org>
2017-12-05 22:54:35 -0600

    day6 prep

---
+++ day6.py
@@ -0,0 +1,16 @@
+#!env python
+
+import aoc
+
+
+def solve1(rows):
+    return None
+
+
+def solve2(rows):
+    return None
+
+if __name__ == '__main__':
+    rows = [row for row in aoc.input_lines(day=6)]
+    print(solve1(rows))
+    print(solve2(rows))
