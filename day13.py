f9fd39d93ff9f2f596bf288841d86fba0fa057b3
Mike Bloy <mike@bloy.org>
2017-12-13 13:11:46 -0600

    more work on day 13

--- day13.py
+++ day13.py
@@ -2,7 +2,6 @@
 
 import aoc
 import collections
-import copy
 import pprint
 
 
@@ -15,37 +14,47 @@ def parse_data(lines):
         traverse = int(traverse)
         if maxlayer < layer:
             maxlayer = layer
-        scannerpositions = (tuple(range(traverse))
-                            + tuple(range(traverse-2, 0, -1)))
-        scanners[layer] = (traverse, scannerpositions)
-    return maxlayer, scanners
+        scanners[layer] = traverse
+    return scanners
 
 
-def position_at(positions, time):
-    return positions[time % len(positions)]
+def position_at(traverse, time):
+    offset = time % ((traverse - 1) * 2)
+    return 2 * (traverse - 1) - offset if offset > traverse - 1 else offset
 
 
-def simulate(layers, maxlayer, start=0):
+def simulate(layers, start=0):
     collisions = []
-    for t in range(maxlayer + 1):
-        if t in layers and position_at(layers[t][1], t+start) == 0:
-            collisions.append((t, layers[t][0]))
+    for t in range(max(layers.keys()) + 1):
+        if t in layers and position_at(layers[t], t+start) == 0:
+            collisions.append((t, layers[t]))
     return sum((a * b) for a,b in collisions)
 
 
+def print_at(layers, time):
+    print("----------")
+    print("Time:", time)
+    for t in range(max(layers.keys()) + 1):
+        if t in layers:
+            print("{0}: {1}/{2}".format(t,
+                                        position_at(layers[t], time),
+                                        layers[t]))
+        else:
+            print("{0}: ---".format(t))
+
 def solve1(data):
-    maxlayer, layers = data
-    return simulate(layers, maxlayer)
+    layers = data
+    return simulate(layers)
 
 
 def solve2(data):
     t = 0
-    maxlayer, layers = data
-    caught = simulate(layers, maxlayer, start=t)
+    layers = data
+    caught = simulate(layers, start=t)
     while caught > 0:
         print(t, caught)
         t += 1
-        caught = simulate(layers, maxlayer, start=t)
+        caught = simulate(layers, start=t)
     return t
 
 
@@ -62,4 +71,6 @@ if __name__ == '__main__':
     #lines = aoc.input_lines(day=13)
     data = parse_data(lines)
     pprint.pprint(solve1(data))
-    # pprint.pprint(solve2(data))
+    pprint.pprint(solve2(data))
+    # for i in range(11):
+        # print_at(data, i)
